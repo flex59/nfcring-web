7dfed97f39cd416c6b81657d2175422e91b1f80b
chrisleach <chrisl@first10.co.uk>
2013-09-02 01:10:19 +0100

    css and js tweaks to gallery page

--- js/nfcringsite.js
+++ js/nfcringsite.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
 		$(".fancybox").fancybox();
 
+		$('.fancybox-thumbs').fancybox({
+			prevEffect : 'none',
+			nextEffect : 'none',
+
+			closeBtn  : true,
+			arrows    : true,
+			nextClick : true,
+
+			helpers : {
+				thumbs : {
+					width  : 80,
+					height : 80
+				}
+			}
+		});
+
 });
