db58e6efe8ec855426eab80b51e338dec4386b1b
chrisleach <chrisl@first10.co.uk>
2013-11-29 13:22:15 +0000

    confirmation box addition

--- bitcoin/js/main.js
+++ bitcoin/js/main.js
@@ -16,5 +16,23 @@ $(function() {
 			offset: -100
 		});
 
+  	$(".paid").fancybox({
+		  maxWidth	: 800,
+			maxHeight	: 600,
+			fitToView	: false,
+			width		: '70%',
+			height		: '70%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
 
+});
+
+
+$(document).on('coinbase_payment_complete', function(event, code){
+
+	$('.paid').click();
+  $("#code").text(code);
 });
