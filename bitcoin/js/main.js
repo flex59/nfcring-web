5d056b067cd95be97ae6d0f63189839c6669da52
chrisleach <chrisl@first10.co.uk>
2013-11-29 12:06:02 +0000

    bitcoin ring page updated images and fixes

---
+++ bitcoin/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+		
+		$(".popup").fancybox({
+			maxWidth	: 800,
+			maxHeight	: 600,
+			fitToView	: false,
+			width		: '70%',
+			height		: '70%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+
+		$('.preorder .button').smoothScroll({
+			offset: -100
+		});
+
+
+});
